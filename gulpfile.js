f4754297f87354f9d2c73086d89e17e543abbb71
RubyCodesYT <81465414+RubyCodesYT@users.noreply.github.com>
2023-01-21 22:51:16 +1300

    new transitions and gulpfile edits

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 
 function buildStyles() {
-    return src('colors.scss')
+    return src(['colors.scss', 'transitions.scss'])
         .pipe(sass())
         .pipe(dest('css'))
 }
 
 function update() {
-    watch(['colors.scss'], buildStyles)
+    watch(['colors.scss', 'transitions.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, update);
